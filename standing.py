189a83bfe14d91335a7c7f03647cbf3bf750565a
Stuart Allan Rutherford <stalruth@gmail.com>
2024-08-14 23:26:45 +1000

    Add dynamic cut calculation

--- standing.py
+++ standing.py
@@ -1,62 +1,61 @@
+import math
+
 round_structures = [
-    (3, 0, 0),
-    (4, 0, 2),
-    (5, 0, 2),
-    (5, 0, 3),
-    (6, 0, 3),
-    (7, 0, 3),
-    (8, 0, 3),
-    (9, 5, 3),
-    (9, 6, 3)
+    (3, 0, False),
+    (4, 0, True),
+    (5, 0, True),
+    (5, 0, True),
+    (6, 0, True),
+    (7, 0, True),
+    (8, 0, True),
+    (9, 5, True),
+    (9, 6, True)
 ]
 
 
 def get_round_count(players, tables):
     index = 0
-    if players <= 8:
-        pass
-    elif players <= 12:
-        index = 1
-    elif players <= 20:
-        index = 2
-    elif players <= 32:
-        index = 3
-    elif players <= 64:
-        index = 4
-    elif players <= 128:
-        index = 5
-    elif players <= 226:
-        index = 6
-    elif players <= 799:
-        index = 7
-    elif players >= 800:
+    if players >= 800:
         index = 8
+    elif players > 226:
+        index = 7
+    elif players > 128:
+        index = 6
+    elif players > 64:
+        index = 5
+    elif players > 32:
+        index = 4
+    elif players > 20:
+        index = 3
+    elif players > 12:
+        index = 2
+    elif players > 8:
+        index = 1
 
-    if index == 0:
-        if len(tables) > 3:
+    if index == 0 and len(tables) > 3:
             index = 1
-    if index == 1:
-        if len(tables) > 4 and len(tables[4]) > 2:
+    if index == 1 and len(tables) > 4 and len(tables[4]) > 2:
             index = 2
-    if index == 2:
-        if len(tables) > 5 and len(tables[5]) > 2:
+    if index == 2 and len(tables) > 5 and len(tables[5]) > 2:
             index = 3
-    if index == 3:
-        if len(tables) > 5 and len(tables[5]) > 4:
+    if index == 3 and len(tables) > 5 and len(tables[5]) > 4:
             index = 4
-    if index == 4:
-        if len(tables) > 6 and len(tables[6]) > 4:
+    if index == 4 and len(tables) > 6 and len(tables[6]) > 4:
             index = 5
-    if index == 5:
-        if len(tables) > 7 and len(tables[7]) > 4:
+    if index == 5 and len(tables) > 7 and len(tables[7]) > 4:
             index = 6
-    if index == 6:
-        if len(tables) > 8 and len(tables[8]) > 4:
+    if index == 6 and len(tables) > 8 and len(tables[8]) > 4:
             index = 7
-    if index == 7:
-        if len(tables) > 15 and len(tables[15]) > 4:
+    if index == 7 and len(tables) > 15 and len(tables[15]) > 4:
             index = 8
-    return round_structures[index]
+
+    (rounds_day1, rounds_day2, has_cut) = round_structures[index]
+    rounds_swiss = rounds_day1 + rounds_day2
+    rounds_cut = 0
+    if has_cut and len(tables) >= (rounds_swiss + 1):
+        rounds_cut = math.log2(len(tables[rounds_swiss]) * 2)
+
+    return (rounds_day1, rounds_day2, rounds_cut)
 
 
 class Standing:
